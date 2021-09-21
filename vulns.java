4aa5ac1d6a9c70efac7900e5a686d4cfdf5dccba
Andrew Mark Feferman <57198537+mfeferman-cx@users.noreply.github.com>
2021-09-21 12:14:29 -0500

    Update vulns.java

--- vulns.java
+++ vulns.java
@@ -18,27 +18,23 @@ public class Vulns {
 		String email = request.getParameter ("email");
 		String password = request.getParameter ("password");
 
+
+		/*
 		String sql = "select * from users where (email ='" + email + "' and password'" + password + "')";
 		Connection connection = pool.getConnection();
 		Statement statement = connection.createStatement();
 		result = statement.executeQuery(sql);
-		
-		/*  this is the solution
-		//String sql = "select * from users where (email ='" + email + "' and password'" + password + "')";
-		String sql = "select * from users where email = ? and password = ? ";
-
-
-		Connection connection = pool.getConnection();
-		//Statement statement = connection.createStatement();
-		PreparedStatement preparedStatement = connection.prepareStatment(sql);
-
-		//Result result = statement.executeQuery(sql);
-		preparedStatement.setString (1, email);
-		preparedStatement.setString (2, password);
-
-		ResultSet result = preparedStatement.executeQuery();
 		*/
 		
+
+		//this is the solution
+		String sql = "select * from users where email = ? and password = ? ";
+		Connection connection = pool.getConnection();
+		PreparedStatement preparedStatement = connection.prepareStatment(sql);
+		preparedStatement.setString (1, email);
+		preparedStatement.setString (2, password);
+		ResultSet result = preparedStatement.executeQuery();
+				
 		if (result.next()) {
 			loggedIn = true;
 			doGet(result,req,response);
